7d254e732dc54b1f0831044885ada40b9d68a502
Khoa Trinh <khoat@intelisysaviation.com>
2025-05-18 16:50:20 +0700

    Change to struct

--- 23-input-scanning/exercises/06-log-parser/main.go
+++ 23-input-scanning/exercises/06-log-parser/main.go
@@ -8,42 +8,88 @@
 
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Log Parser from Stratch
 //
-//  You've watched the lecture. Now, try to create the same
-//  log parser program on your own. Do not look at the lecture,
-//  and the existing source code.
-//
+//	You've watched the lecture. Now, try to create the same
+//	log parser program on your own. Do not look at the lecture,
+//	and the existing source code.
 //
 // EXPECTED OUTPUT
 //
-//  go run main.go < log.txt
+//	go run main.go < log.txt
 //
-//   DOMAIN                             VISITS
-//   ---------------------------------------------
-//   blog.golang.org                        30
-//   golang.org                             10
-//   learngoprogramming.com                 20
+//	 DOMAIN                             VISITS
+//	 ---------------------------------------------
+//	 blog.golang.org                        30
+//	 golang.org                             10
+//	 learngoprogramming.com                 20
 //
-//   TOTAL                                  60
-//
-//
-//  go run main.go < log_err_missing.txt
-//
-//   wrong input: [golang.org] (line #3)
+//	 TOTAL                                  60
 //
 //
-//  go run main.go < log_err_negative.txt
+//	go run main.go < log_err_missing.txt
 //
-//   wrong input: "-100" (line #3)
+//	 wrong input: [golang.org] (line #3)
 //
 //
-//  go run main.go < log_err_str.txt
+//	go run main.go < log_err_negative.txt
 //
-//   wrong input: "FOUR" (line #3)
+//	 wrong input: "-100" (line #3)
+//
+//
+//	go run main.go < log_err_str.txt
+//
+//	 wrong input: "FOUR" (line #3)
 //
 // ---------------------------------------------------------
+type parse struct {
+	DOMAINS map[string]int
+	domain  []string
+	total   int
+	line    int
+}
 
 func main() {
+
+	data := bufio.NewScanner(os.Stdin)
+
+	p := parse{
+		DOMAINS: make(map[string]int),
+	}
+
+	line := 0
+	for data.Scan() {
+		line++
+		parts := strings.Split(data.Text(), " ")
+		if len(parts) != 2 {
+			fmt.Printf("wrong input: %q (line #%d)\n", data.Text(), line)
+			continue
+		}
+		domain := parts[0]
+		visits, err := strconv.Atoi(parts[1])
+		if err != nil {
+			fmt.Printf("wrong input: %q (line #%d)\n", parts[1], line)
+			continue
+		}
+		p.DOMAINS[domain] += visits
+		p.domain = append(p.domain, domain)
+		p.total += visits
+	}
+	fmt.Printf("%-30s %s\n", "DOMAIN", "VISITS")
+	fmt.Println("---------------------------------------------")
+
+	for _, domain := range p.domain {
+		fmt.Printf("%-30s %d\n", domain, p.DOMAINS[domain])
+	}
+	fmt.Println("---------------------------------------------")
+	fmt.Printf("%-30s %d\n", "TOTAL", p.total)
 }
